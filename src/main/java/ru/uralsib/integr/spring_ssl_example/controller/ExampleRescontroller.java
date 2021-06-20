a5f93b9900be8715b544f120960b138442ce9d7a
Alexander Lyapustin <alexl1967@mail.ru>
2021-06-20 21:57:58 +0300

    add  src/main/java/ru/uralsib/integr/spring_ssl_example/controller/ExampleRescontroller.java

--- src/main/java/ru/uralsib/integr/spring_ssl_example/controller/ExampleRescontroller.java
+++ src/main/java/ru/uralsib/integr/spring_ssl_example/controller/ExampleRescontroller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleRescontroller {
-
+//https://www.youtube.com/watch?v=rm9OKTSm-4A
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World! With new keys!";
